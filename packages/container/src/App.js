5721bb271f3a9ffa26a2888e1358f92998aaaf32
Edgar Olivar <ext_emolivars@falabella.cl>
2021-11-05 16:30:14 -0300

    ✨ dashboard config

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,21 +1,37 @@
-import React, { lazy, Suspense, useState } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React, { lazy, Suspense, useState, useEffect } from 'react';
+import {
+  BrowserRouter,
+  Route,
+  Switch,
+  Redirect,
+  Router,
+} from 'react-router-dom';
 import { createGenerateClassName, StylesProvider } from '@material-ui/styles';
 import Header from './components/Header';
 import { Progress } from './components/Progress';
+import { createBrowserHistory } from 'history';
 
 const MarketingLazy = lazy(() => import('./components/MarketingApp'));
 const AuthLazy = lazy(() => import('./components/AuthApp'));
+const DashboardLazy = lazy(() => import('./components/DashboardApp'));
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'co',
 });
 
+const history = createBrowserHistory();
+
 export default () => {
   const [isSignIn, setIsSignIn] = useState(false);
 
+  useEffect(() => {
+    if (isSignIn) {
+      history.push('/dashboard');
+    }
+  }, [isSignIn]);
+
   return (
-    <BrowserRouter>
+    <Router history={history}>
       <>
         <StylesProvider generateClassName={generateClassName}>
           <Header isSignedIn={isSignIn} onSignOut={() => setIsSignIn(false)} />
@@ -27,11 +43,16 @@ export default () => {
                   <AuthLazy onSignIn={() => setIsSignIn(true)} />
                 )}
               />
+              <Route path="/dashboard">
+                {!isSignIn && <Redirect to="/" />}
+                <DashboardLazy />
+              </Route>
+
               <Route path="/" component={MarketingLazy} />
             </Switch>
           </Suspense>
         </StylesProvider>
       </>
-    </BrowserRouter>
+    </Router>
   );
 };
---
+++ packages/dashboard/src/components/Test.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Test</h1>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Test',
+  data() {
+    return {
+      message: 'Welcome to Your Vue.js App',
+    };
+  },
+};
+</script>
+
+<style></style>
