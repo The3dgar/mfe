28379f7395ee8c54b98844ca84a346ab027052c1
Edgar Olivar <ext_emolivars@falabella.cl>
2021-11-05 11:06:04 -0300

    ✨ configs for 2 microfrontend

--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -3,17 +3,17 @@ import ReactDom from 'react-dom';
 import App from './App';
 import { createMemoryHistory, createBrowserHistory } from 'history';
 
-// Mount function to start up the app
-const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
+const mount = (el, { onNavigate, defaultHistory, initialPath, onSignIn }) => {
   const history =
     defaultHistory ||
     createMemoryHistory({
       initialEntries: [initialPath],
     });
-    
+
   if (onNavigate) history.listen(onNavigate);
 
-  ReactDom.render(<App history={history} />, el);
+  ReactDom.render(<App history={history} onSignIn={onSignIn} />, el);
+
   return {
     onParentNavigate: ({ pathname: nextPathname }) => {
       const { pathname } = history.location;
@@ -24,9 +24,8 @@ const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
   };
 };
 
-//  if we are in dev & in isolation
 if (process.env.NODE_ENV === 'development') {
-  const devRoot = document.querySelector('#_marketing-dev-root');
+  const devRoot = document.querySelector('#_auth-dev-root');
 
   if (devRoot) {
     mount(devRoot, { defaultHistory: createBrowserHistory() });
--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,20 +1,35 @@
+import React, { lazy, Suspense, useState } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { createGenerateClassName, StylesProvider } from '@material-ui/styles';
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
 import Header from './components/Header';
-import MarketingApp from './components/MarketingApp';
+import { Progress } from './components/Progress';
+
+const MarketingLazy = lazy(() => import('./components/MarketingApp'));
+const AuthLazy = lazy(() => import('./components/AuthApp'));
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'co',
 });
 
 export default () => {
+  const [isSignIn, setIsSignIn] = useState(false);
+
   return (
     <BrowserRouter>
       <>
         <StylesProvider generateClassName={generateClassName}>
-          <Header />
-          <MarketingApp />
+          <Header isSignedIn={isSignIn} onSignOut={() => setIsSignIn(false)} />
+          <Suspense fallback={<Progress />}>
+            <Switch>
+              <Route
+                path="/auth"
+                component={() => (
+                  <AuthLazy onSignIn={() => setIsSignIn(true)} />
+                )}
+              />
+              <Route path="/" component={MarketingLazy} />
+            </Switch>
+          </Suspense>
         </StylesProvider>
       </>
     </BrowserRouter>
